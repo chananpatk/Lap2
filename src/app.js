a3d69680d82c3d201675cf890378b5e18f71a219
Chananpat - <macbookpro@Chananpat-2.local>
2024-02-03 16:03:33 +0700

    add nodemon

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log('Hello from app.js!')
+const express = require('express');
+const app = express();
+// Define a route for the root path ("/")
+app.get('/',(req, res) => {
+    res.send('Hello World!!!TEST');
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000!');
+})
