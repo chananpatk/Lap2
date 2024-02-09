f27ccbf04cfb39f39f271e91eeff962011911e97
chananpatk <chananpat_k@cmu.ac.th>
2024-02-09 15:53:52 +0700

    เพิ่ม body-parser ในการใช้ req.body  และ แก้ไข '/test/:id/name/:name'

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');  // เพิ่ม body-parser เข้ามา
 const app = express();
+
+app.use(bodyParser.json());  // เรียกใช้ body-parser middleware เพื่อแปลงข้อมูลจาก JSON เป็น JavaScript object
+
 // Define a route for the root path ("/")
 app.get('/',(req, res) => {
-    res.send('Hello World!!!3210');
+    res.send('Hello World!!!5');
 });
 
 app.get('/test',(req ,res) => {
@@ -13,11 +17,19 @@ app.get('/test',(req ,res) => {
     res.send(output); 
  });
     
- app.get('/test/:id/:name', (req, res) => {
+ app.get('/test/:id/name/:name', (req, res) => {
     const id = req.params.id;
     const name = req.params.name;
     res.send(`id: ${id} and name: ${name}`);
- });
+ }); 
+
+ app.post('/test', (req, res) => {
+    const obj = req.body;             // req.body ต้องมีการเรียกใข้  body-parser ก่อน
+    const id = obj.id;
+    const name = obj.name;
+    res.send(`id: ${id} and name: ${name}`);
+});
+
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000!');
