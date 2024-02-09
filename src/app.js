89233d494c5d334e8a46f1648fb713365ff17789
chananpatk <chananpat_k@cmu.ac.th>
2024-02-09 14:33:54 +0700

    1.3

--- src/app.js
+++ src/app.js
@@ -2,16 +2,18 @@ const express = require('express');
 const app = express();
 // Define a route for the root path ("/")
 app.get('/',(req, res) => {
-    res.send('Hello World!!!');
+    res.send('Hello World!!!321');
 });
 
 app.get('/test',(req ,res) => {
 const id = req.query.id;
 
-const output = `id: ${id}` ;
-res.send(output); 
-});
-
+const name = req.query.name;
+const output = `id: ${id} and name: ${name}` ;
+    res.send(output); 
+ });
+    
+ 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000!');
 });
