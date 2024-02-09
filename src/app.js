d00b69159b2ee51795a1814b551eb47cc441ff17
chananpatk <chananpat_k@cmu.ac.th>
2024-02-09 14:43:06 +0700

    1.6 add case No_name

--- src/app.js
+++ src/app.js
@@ -8,12 +8,12 @@ app.get('/',(req, res) => {
 app.get('/test',(req ,res) => {
 const id = req.query.id;
 
-const name = req.query.name;
+const name = req.query.name || 'No name';
 const output = `id: ${id} and name: ${name}` ;
     res.send(output); 
  });
     
- 
+
 app.listen(3000, () => {
     console.log('Server is listening on port 3000!');
 });
